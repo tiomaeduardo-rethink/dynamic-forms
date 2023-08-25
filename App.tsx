6480b950a3ac62a3dcd80e91d50cbb5828e58f7b
tiomaeduardo-rethink <eduardo.tioma@rethink.dev>
2023-08-25 11:15:32 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {Text} from 'react-native-paper';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>hello</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
